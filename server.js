aea154976c27d261ac8683c7fc32dc042c1802f7
pankaj485 <pankajsamar2@gmail.com>
2026-01-31 01:03:00 +0530

    feat: add very basic ws server

---
+++ server.js
@@ -0,0 +1,36 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const PORT = 8080;
+const wss = new WebSocketServer({ port: PORT });
+
+// basic server setup
+wss.on("connection", (socket, request) => {
+  const ipAddr = request.socket.remoteAddress;
+  console.log("connected with ", ipAddr);
+
+  socket.on("message", (bufferData) => {
+    const data = bufferData.toString();
+
+    wss.clients.forEach((client) => {
+      const isConnectionOpen = client.readyState === WebSocket.OPEN;
+      if (isConnectionOpen) {
+        client.send(
+          JSON.stringify({
+            success: true,
+            message: data,
+          }),
+        );
+      }
+    });
+  });
+
+  socket.on("error", () => {
+    console.log("Error on socket. Client address: ", ipAddr);
+  });
+
+  socket.on("close", () => {
+    console.log("Connection closed with Client address: ", ipAddr);
+  });
+});
+
+console.log(`Websocket server running on PORT: ${PORT}`);
